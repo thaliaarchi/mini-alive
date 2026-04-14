eb53fc23abcea688bfd053515ef3e1d758333d88
Thalia Archibald <thalia@archibald.dev>
2026-04-14 13:50:01 -0600

    Test whitespace parsing

--- src/syntax/tests.rs
+++ src/syntax/tests.rs
@@ -166,6 +166,28 @@ while.end:
   ret i16 %c.0
 }
 ";
+
+    let popcnt_numbered = "\
+define i16 @popcnt(i16 %0) {
+1:
+  br label %2
+
+2:
+  %3 = phi i16 [ %0, %1 ], [ %8, %6 ]
+  %4 = phi i16 [ 0, %1 ], [ %9, %6 ]
+  %5 = icmp eq i16 %3, 0
+  br i1 %5, label %10, label %6
+
+6:
+  %7 = add i16 %3, -1
+  %8 = and i16 %3, %7
+  %9 = add i16 %4, 1
+  br label %2
+
+10:
+  ret i16 %4
+}
+";
     let tests = [
         (agg, agg),
         (popcnt, popcnt),
@@ -187,27 +209,15 @@ define i16 @popcnt(i16) {
   ret i16 %4
 }
 ",
+            popcnt_numbered,
+        ),
+        (
             "\
-define i16 @popcnt(i16 %0) {
-1:
-  br label %2
-
-2:
-  %3 = phi i16 [ %0, %1 ], [ %8, %6 ]
-  %4 = phi i16 [ 0, %1 ], [ %9, %6 ]
-  %5 = icmp eq i16 %3, 0
-  br i1 %5, label %10, label %6
-
-6:
-  %7 = add i16 %3, -1
-  %8 = and i16 %3, %7
-  %9 = add i16 %4, 1
-  br label %2
-
-10:
-  ret i16 %4
-}
+define i16@popcnt(i16){br label%2phi i16[%0,%1],[%8,%6]phi i16[0,%1],[%9,%6]icmp
+eq i16%3,0br i1%5,label%10,label%6add i16%3,-1and i16%3,%7add i16%4,1br label%2
+ret i16%4}
 ",
+            popcnt_numbered,
         ),
         (
             "declare i16 @popcnt(i16 %x)\n",
